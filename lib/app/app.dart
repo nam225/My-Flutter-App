df76e9ed945c58cf58e1f76a1909787d925788e7
Dao Cu Nam <daocunam225@gmail.com>
2024-09-16 22:56:28 +0700

    step 4 : create styles, theme manager

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/presentation/theme_manager.dart';
 
 class MyApp extends StatefulWidget {
   MyApp._internal();
@@ -15,6 +16,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
   }
 }
